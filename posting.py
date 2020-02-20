4bd0f4ed15ab54eb30a5a89b27e579b6118ba308
Ryan Hahn <ryanhahn239@gmail.com>
2020-02-20 15:41:43 -0800

    get_important

--- posting.py
+++ posting.py
@@ -1,9 +1,8 @@
 class Posting(self):
     def __init__(self):
+        self.docID = ""
         self.url = ""
         self.total_count = 0
         self.target_count = 0
         self.idf = 0
-    
-    def write(self):
-        r;oji
+        self.importance = False
--- tokenizer.py
+++ tokenizer.py
@@ -29,12 +29,34 @@ def extract_text(json_data):
     except Exception as e:
         print(e)
 
-def tokenize(s):
+# <strong> tags as important? Or only <bold>, <h1>, <h2>, <h3>, and <title>
+def extract_important(json_data):
+    try:
+         if json_data:
+            web_content = json_data["content"]
+            soup = BeautifulSoup(web_content, "html.parser")
+            count = 0
+            text = ""
+
+            for script in soup(["script", "style"]):
+                script.decompose()
+
+            for script in soup(["strong", "bold", "h1", "h2", "h3", "title"]):
+                text += script.get_text() + " "
+            
+            lines = (line.strip() for line in text.splitlines())
+            chunks = (phrase.strip() for line in lines for phrase in line.split(" "))
+            
+            text = ' '.join(chunk for chunk in chunks if chunk)
+            return text
+
+    except Exception as e:
+        print(e)
+
+
+def tokenizer(s):
     token = set(re.sub('[^a-z0-9]', ' ', s.lower()).split())
-    
-
     return token
-
 
 path = "C:/Users/raynh/Desktop/assignment3/analyst/www_informatics_uci_edu/0a3175bebaa8bab4bc69961115bf3cebf8c6337ae6a62394c8e5e2d5509ddcee.json"
 
@@ -42,5 +64,6 @@ with open(path) as json_file:
     data = json.load(json_file)
     #print(data["encoding"])
     output = extract_text(data)
-    tokens = tokenize(output)
-    print(tokens)
+    tokens = tokenizer(output)
+    #print(tokens)
+    print(extract_important(data))
