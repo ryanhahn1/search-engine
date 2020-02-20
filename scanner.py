dc1d9e00b25a4aff6e363000b57767bbd4553f8f
Justine Woo <justinewoo8@gmail.com>
2020-02-20 01:12:42 -0800

    gets files

--- scanner.py
+++ scanner.py
@@ -1,5 +1,20 @@
+import os
+
 def scan_documents():
     simhashes = set() #simhash object
     tokens = dict() #key = word, value = number of instances
     pagecount = 0
     longest = ['', 0]
+
+def get_files(main_dir):
+	for file in os.listdir(main_dir):
+		path = os.path.join(main_dir, file)
+		if os.path.isdir(path):
+			get_files(path)
+		elif os.path.isfile(path):
+			print(file)
+
+
+if __name__ == '__main__':
+	file_location = "ANALYST"
+	get_files(os.path.join(os.path.dirname(os.getcwd()), file_location))
