76f9ad767a38e83e9998afc795c710460c72201e
Ryan Hahn <ryanhahn239@gmail.com>
2020-02-21 16:12:17 -0800

    updated postings

--- posting.py
+++ posting.py
@@ -1,8 +1,18 @@
+import math
+
 class Posting(self):
-    def __init__(self):
-        self.docID = ""
-        self.url = ""
-        self.total_count = 0
-        self.target_count = 0
+    def __init__(self, id, frequency, important, wordcount)
+        self.docID = id
+        # self.postings
+        self.word_count = wordcout
+        self.term_freq = frequency
         self.idf = 0
-        self.importance = False
+        self.importance = important
+    
+    # calculates and sets the idf attribute
+    def set_idf(total_docs, total_with_term):
+        self.idf = log( (total_docs / total_with_term) )
+
+    # calculates and returns the tf-idf
+    def get_tfidf():
+        return ( (term_freq / word_count) / self.idf )
