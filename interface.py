a14ccb19a9c5a23a923bcc201af9ace3bc3332eb
Victor Meng <mengvf@uci.edu>
2020-02-26 00:46:33 -0800

    UI works now but very slow

--- interface.py
+++ interface.py
@@ -1,25 +1,19 @@
 from flask import Flask, render_template, escape, request, url_for, redirect
 from search import Search
-import main
+from main import get_results
 
 app = Flask(__name__)
 
 app.config['SECRET_KEY'] = 'aab774a98e91dbc255f8ac641798d3de'
 
-results = []
-
 @app.route('/', methods =['GET', 'POST'])
 @app.route('/home', methods=['GET', 'POST'])
 def home():
     search = Search()
+    results = []
     if search.validate_on_submit():
-        results.append(search.query.data)
-        return redirect(url_for('output'))
-    return render_template('home.html', title = 'Home', search = search)
-
-@app.route('/results')
-def output():
-    return render_template('results.html', title = 'Results',results = results, size = size)
+        results = get_results(search.query.data)
+    return render_template('home.html', title = 'Home', search = search, results = results)
 
 if __name__ == '__main__':
     app.run(debug=True)
--- main.py
+++ main.py
@@ -5,6 +5,16 @@ import os
 import json 
 
 
+def get_results(input):
+	main_path = os.path.dirname(os.getcwd()) + "/index/main.txt"
+	url_index_file = os.path.dirname(os.getcwd()) + "/index/url_index.json"
+	with open(url_index_file) as json_file:
+		url_index = json.load(json_file)
+	query = query_processor(input)
+	ranking = []
+	for docid, value in sorted(find_all_boolean(query, main_path).items(), key=lambda x: -x[1]):
+		ranking.append(url_index[str(docid)])
+	return ranking
 
 if __name__ == '__main__':
 	main_path = os.path.dirname(os.getcwd()) + "/index/main.txt"
