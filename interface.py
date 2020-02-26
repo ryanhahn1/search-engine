b9a323ab3c6291c16c57e243357030f7677dde37
Victor Meng <mengvf@uci.edu>
2020-02-25 23:57:27 -0800

    a lot of UI

--- interface.py
+++ interface.py
@@ -1,18 +1,21 @@
-from flask import Flask, render_template, escape, request, url_for
-# import main
+from flask import Flask, render_template, escape, request, url_for, redirect
+from search import Search
+import main
 
 app = Flask(__name__)
 
-# results = getResults() a list of results
+app.config['SECRET_KEY'] = 'aab774a98e91dbc255f8ac641798d3de'
 
-@app.route('/')
-@app.route('/home')
+results = []
+
+@app.route('/', methods =['GET', 'POST'])
+@app.route('/home', methods=['GET', 'POST'])
 def home():
-    return render_template('home.html', title = 'Home')
-
-# results = getResults() a list of results
-results = ["hello", "bye", "why"]
-size = len(results)
+    search = Search()
+    if search.validate_on_submit():
+        results.append(search.query.data)
+        return redirect(url_for('output'))
+    return render_template('home.html', title = 'Home', search = search)
 
 @app.route('/results')
 def output():
