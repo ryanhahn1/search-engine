0780d6a8c8426d95cd0ab3785a19b81542a5ada8
Victor Meng <mengvf@uci.edu>
2020-03-11 23:41:47 -0700

    added comments to home.html, search.py, ranking.py, layout.html

--- search.py
+++ search.py
@@ -2,6 +2,7 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired
 
+#use FlaskForm to create the backend of search engine
 class Search(FlaskForm):
     query = StringField('Search', validators=[DataRequired()])
     submit = SubmitField('Enter')
